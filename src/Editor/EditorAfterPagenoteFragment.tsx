b2775e8e80808c9a2d1e775458b9e99faaeaf7c5
yoophoon <2420074212@qq.com>
2024-06-30 13:29:54 +0800

    扩展logo

---
+++ src/Editor/EditorAfterPagenoteFragment.tsx
@@ -0,0 +1,89 @@
+    import { Box, Divider } from "@mui/material";
+import EditorTitle from "./components/EditorTitle";
+import EditorToolBar from "./components/EditorToolBar";
+import EditorContent from "./components/EditorContent";
+// import zIndex from "@mui/material/styles/zIndex";
+import { EditorContext } from ".";
+import { useContext, useEffect, useRef, useState } from "react";
+const style = {
+    boxShadow: 24,
+    p: 2,
+    'resize':"vertical",
+    overflow:'auto',
+    minWidth:'100%',
+    minHeight:'200px',
+    height:'200px',
+    zIndex:999,
+  };
+
+export default function EditorAfterPagenoteFragment() {
+  const editorContext = useContext(EditorContext)
+  if (editorContext == null) return
+  const { editorStatus ,setEditorStatus} = editorContext
+  const [showTitleAndTools,setShowTitleTools]=useState({
+    showTitle:editorStatus.showTitle,
+    showTools:editorStatus.showTools,
+  })
+  
+  useEffect(()=>{
+    setShowTitleTools(setting=>({
+      ...setting,
+      showTitle:editorStatus.showTitle,
+      showTools:editorStatus.showTools,
+    }))
+  },[editorStatus.showTitle,editorStatus.showTools])
+  
+  const handlerMouseEnter = (e:React.MouseEvent) => {
+    setShowTitleTools({
+      showTitle:true,
+      showTools:true,
+    })
+  }
+
+  const handlerMouseLeave=(e:React.MouseEvent)=>{
+    setShowTitleTools({
+      showTitle:editorStatus.showTitle,
+      showTools:editorStatus.showTools,
+    })
+  }
+
+  const handlerMouseUp=(e:React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
+    console.log('resize...',e)
+    console.log(editorStatus)
+    const target=e.target as typeof e.currentTarget
+    setEditorStatus(editorStatus=>({
+      ...editorStatus,
+      editorHeight:target.style.height,
+    }))
+  }
+
+  return (<Box
+    sx={{
+      ...style,
+      boxSizing:'border-box',
+      // width:editorStatus.editorWidth,
+      // maxWidth:editorStatus.editorWidth,
+      height:editorStatus.editorHeight,
+      float:'left',
+    }}
+    bgcolor={"background.default"}
+    color={'text.primary'}
+    id='pagenoteEditor'
+    onMouseEnter={handlerMouseEnter}
+    onMouseLeave={handlerMouseLeave}
+    onMouseUp={e=>handlerMouseUp(e)}
+  >
+    {/* <EditorSetting/> */}
+    {
+      showTitleAndTools.showTitle && <>
+      <EditorTitle />
+      <Divider aria-hidden="false" />
+    </>}
+    {
+      showTitleAndTools.showTools && <>
+      <EditorToolBar />
+      <Divider aria-hidden="false" />
+    </>}
+    <EditorContent showTitleAndTools={showTitleAndTools}/>
+  </Box>)
+}
